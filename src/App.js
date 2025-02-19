cde32b156f4af54bbfdf18f1cc879fa68b56abd3
Adam I. Horvath <horvatha.dev@gmail.com>
2025-02-19 20:11:08 +0100

    first welcome design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Import the CSS file
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="name__container">
+        <div className="name">
+          Horváth<span className="bold">&nbsp;Ádám&nbsp;István</span>
+        </div>
+      </div>
+      <div className="emoji">👋</div>
     </div>
   );
 }
